b074b7459b920d16698dd5d0707a958870a61f6d
julianmoder <julianmo@live.de>
2025-06-02 14:22:00 +0200

    dev commit
    - search field optimization
    - filter settings bug fix

--- js/handgemacht-main.js
+++ js/handgemacht-main.js
@@ -5,7 +5,7 @@
 //START app 
 const app = {
 	title: 'Unikathek',
-	version: 'beta 1.8 25/02/17',
+	version: 'beta 1.9 25/06/02',
 	dev: false,
 	stats: false,
 	viewerMode: false,
@@ -1765,6 +1765,14 @@ const app = {
 								"imageAlt" : "Musikanten bei den WAA-Protesten",
 								"imageCaption" : "Als in den 1980er-Jahren eine atomare Wiederaufbereitungsanlage bei Wackersdorf gebaut werden sollte, regte sich in der Oberpfalz heftiger Widerstand. Ein WAA-Gegner erzählt von seinen Erlebnissen und zeigt verschiedene Objekte, die im Widerstand eine Rolle spielten.",
 								"type" : "description+tour"
+							},
+							{
+								"content" : "Kirwa in der Oberpfalz",
+								"fileCopyright" : "Michaela Stauber",
+								"filename" : "kir tour - Kirwa Austanzen und Musik.jpg",
+								"imageAlt" : "Kirwa Austanzen mit Musikern",
+								"imageCaption" : "[ändern] „Kirwa ist ein Lebensg’fühl“, heißt es auf einem Internetauftritt zum Kulturerbe Kirwa. Doch was ist das genau? Was gehört dazu? Was macht die Kirwa in der Oberpfalz aus? Die beiden kirwaerprobten Jugendlichen Franzi und Florian zeigen es dir und geben Einblicke hinter die Kulissen.",
+								"type" : "description+tour"
 							}, 
 							{
 								"content" : "Besondere Objekte",
@@ -1824,6 +1832,14 @@ const app = {
 								"imageAlt" : "",
 								"imageCaption" : "",
 								"type" : "link"
+							}, 
+							{
+								"content" : "Förderung",
+								"fileCopyright" : "",
+								"filename" : "",
+								"imageAlt" : "",
+								"imageCaption" : "",
+								"type" : "headline"
 							},
 							{
 								"content" : "Das Projekt wurde vom 1. Juni 2022 bis zum 30. Juni 2025 im Rahmen der Heimat-Digital-Regional Förderrichtlinie des Bayerischen Staatsministeriums der Finanzen und für Heimat gefördert.",
@@ -1848,6 +1864,14 @@ const app = {
 								"imageAlt" : "Bayerisches Staatsministerium der Finanzen und für Heimat als Förderer Logo",
 								"imageCaption" : "",
 								"type" : "image+patronage"
+							}, 
+							{
+								"content" : " ",
+								"fileCopyright" : "",
+								"filename" : "",
+								"imageAlt" : "",
+								"imageCaption" : "",
+								"type" : "headline"
 							},
 							{
 								"content" : "Kontakt",
@@ -3092,28 +3116,54 @@ const app = {
 					autocompleteList.classList.add(app.collectionViewer.search.buttonSetup.colors.tab.text);
 	
 					currentFocus = -1;
-	
+					
+					const debugArray = [];
+					app.dev && console.log('dev --- search debug array: ', array);
+
 					for(let index in array){
 						let name = array[index].name;
-						if (name.substr(0, inputValue.length).toUpperCase() == inputValue.toUpperCase()) {
-							let listItemEl = document.createElement('div');
-							listItemEl.innerHTML = "<strong>" + name.substr(0, inputValue.length) + "</strong>";
-							listItemEl.innerHTML += name.substr(inputValue.length);
-							array[index].visibility === 'hidden' ? listItemEl.innerHTML += ' (gefiltert)' : '';
-							listItemEl.innerHTML += "<input type='hidden' value='" + name + "'>";
-							autocompleteList.appendChild(listItemEl);
-							if(array[index].visibility === 'hidden'){
-								listItemEl.classList.add('disabled');
-								continue;
+						for (var i = 0; i < name.length; i++) {
+							if (name.substring(i, i+inputValue.length).toUpperCase() == inputValue.toUpperCase()) {
+								let listItemEl = document.createElement('div');
+								listItemEl.innerHTML = name.substring(0, i);
+								listItemEl.innerHTML += "<strong>" + name.substring(i, i+inputValue.length) + "</strong>";
+								listItemEl.innerHTML += name.substring(i+inputValue.length);
+								array[index].visibility === 'hidden' ? listItemEl.innerHTML += ' (gefiltert)' : '';
+								listItemEl.innerHTML += "<input type='hidden' value='" + name + "'>";
+								autocompleteList.appendChild(listItemEl);
+								if(array[index].visibility === 'hidden'){
+									listItemEl.classList.add('disabled');
+									continue;
+								}
+								listItemEl.addEventListener('click', function(e) {
+									element.value = this.getElementsByTagName("input")[0].value;
+									app.collectionViewer.search.removeAutoCompleteList();
+									app.gui.toolbar.buttonActionSlide(app.collectionViewer.search.button.element);
+									app.collectionViewer.search.executeRequest(name);
+								});
 							}
-							listItemEl.addEventListener('click', function(e) {
-								element.value = this.getElementsByTagName("input")[0].value;
-								app.collectionViewer.search.removeAutoCompleteList();
-								app.gui.toolbar.buttonActionSlide(app.collectionViewer.search.button.element);
-								app.collectionViewer.search.executeRequest(name);
-							});
 						}
+						// if (name.substring(0, inputValue.length).toUpperCase() == inputValue.toUpperCase()) {
+						// 	let listItemEl = document.createElement('div');
+						// 	listItemEl.innerHTML = "<strong>" + name.substring(0, inputValue.length) + "</strong>";
+						// 	listItemEl.innerHTML += name.substring(inputValue.length);
+						// 	array[index].visibility === 'hidden' ? listItemEl.innerHTML += ' (gefiltert)' : '';
+						// 	listItemEl.innerHTML += "<input type='hidden' value='" + name + "'>";
+						// 	autocompleteList.appendChild(listItemEl);
+							// if(array[index].visibility === 'hidden'){
+							// 	listItemEl.classList.add('disabled');
+							// 	continue;
+							// }
+							// listItemEl.addEventListener('click', function(e) {
+							// 	element.value = this.getElementsByTagName("input")[0].value;
+							// 	app.collectionViewer.search.removeAutoCompleteList();
+							// 	app.gui.toolbar.buttonActionSlide(app.collectionViewer.search.button.element);
+							// 	app.collectionViewer.search.executeRequest(name);
+							// });
+						// }
 					}
+
+					app.dev && console.log('dev --- search debug output: ', debugArray);
 
 					if(!autocompleteList.hasChildNodes()) {
 						let listItemEl = document.createElement('div');
@@ -11689,7 +11739,7 @@ const app = {
 		// from: https://stackoverflow.com/questions/11381673/detecting-a-mobile-browser
 		let check = false;
 		(function(a){
-			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) {
+			if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substring(0,4))) {
 				check = true;
 			}
 		})(navigator.userAgent||navigator.vendor||window.opera);
