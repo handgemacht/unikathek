5a105282e59fdcb64f8a9df70df8ed9951cf09b8
julianmoder <julianmo@live.de>
2025-07-16 13:54:02 +0200

    feat(api): setup api module
    -api module setup

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
 
 export const db = new PrismaClient();
-
-export * from '@prisma/client'; // <- Exportiert alle Typen, z.B. CulturalObject
+export * from '@prisma/client';
