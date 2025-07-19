d714221597f8dde6d7893cdf229187cdafd09b2e
julianmoder <julianmo@live.de>
2025-07-19 20:42:02 +0200

    typed mesh ref

--- apps/explorer/src/Viewport3DTest.tsx
+++ apps/explorer/src/Viewport3DTest.tsx
@@ -1,8 +1,10 @@
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber'
+import { useRef, type MutableRefObject } from 'react';
+import { useFrame } from '@react-three/fiber';
+import type { Mesh } from 'three';
+import type * as THREE from 'three';
 
 export function Viewport3DTest() {
-  const myMesh = useRef()
+  const myMesh = useRef<THREE.Mesh>(null!) as MutableRefObject<Mesh>;
 
   useFrame(({ clock }) => {
     myMesh.current.rotation.x = clock.elapsedTime
